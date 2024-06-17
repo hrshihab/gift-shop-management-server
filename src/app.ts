ea48cef697dd33f066c314d5e795db602fe63f33
hrshihab <hrshihab10@gmail.com>
2024-06-17 18:31:10 +0600

    env update

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: ['http://localhost:5173'],
+        origin: [
+            'http://localhost:5173',
+            'https://gift-gallery-three.vercel.app',
+        ],
         credentials: true,
     }),
 );
